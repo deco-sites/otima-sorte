555cd37b9f000c68fb90c5a04a0aea8b7edd4b4e
FelipeCSRosa <ffelipecs.rosa@gmail.com>
2024-01-24 14:47:19 -0300

    feat: footer WIP

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -11,6 +11,7 @@ import Social from "$store/components/footer/Social.tsx";
 import Newsletter from "$store/islands/Newsletter.tsx";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import PoweredByDeco from "apps/website/components/PoweredByDeco.tsx";
+import RichText from "$store/sections/Content/RichText.tsx";
 
 export type Item = {
   label: string;
@@ -82,269 +83,249 @@ export interface Layout {
   };
 }
 
+interface Image {
+  image: ImageWidget;
+  alt?: string;
+}
+
 export interface Props {
-  logo?: {
-    image: ImageWidget;
-    description?: string;
-  };
   newsletter?: {
     title?: string;
     /** @format textarea */
     description?: string;
     form?: NewsletterForm;
   };
+  logo?: Image;
+  socialIcons?: Image[];
   sections?: Section[];
-  social?: {
+  payment?: {
     title?: string;
-    items: SocialItem[];
+    icons?: Image[];
   };
-  payments?: {
+  security?: {
     title?: string;
-    items: PaymentItem[];
+    icons?: Image[];
   };
-  mobileApps?: MobileApps;
-  regionOptions?: RegionOptions;
-  extraLinks?: Item[];
-  backToTheTop?: {
-    text?: string;
-  };
-  layout?: Layout;
+  poweredIcons?: Image[];
+  /** @format textarea */
+  textBottom?: string;
 }
 
-function Footer({
-  logo,
-  newsletter = {
+const DEFAULT_PROPS = {
+  newsletter: {
     title: "Newsletter",
     description: "",
     form: { placeholder: "", buttonText: "", helpText: "" },
   },
-  sections = [{
-    "label": "Sobre",
-    "items": [
-      {
-        "href": "/quem-somos",
-        "label": "Quem somos",
-      },
-      {
-        "href": "/termos-de-uso",
-        "label": "Termos de uso",
-      },
-      {
-        "href": "/trabalhe-conosco",
-        "label": "Trabalhe conosco",
-      },
-    ],
-  }, {
-    "label": "Atendimento",
-    "items": [
-      {
-        "href": "/centraldeatendimento",
-        "label": "Central de atendimento",
-      },
-      {
-        "href": "/whatsapp",
-        "label": "Fale conosco pelo WhatsApp",
-      },
-      {
-        "href": "/trocaedevolucao",
-        "label": "Troca e devolução",
-      },
-    ],
-  }],
-  social = {
-    title: "Redes sociais",
-    items: [{ label: "Instagram", link: "/" }, { label: "Tiktok", link: "/" }],
+  logo: {
+    image: "https://fakeimg.pl/264x66/625afa/ffffff",
+    alt: "logo",
   },
-  payments = {
-    title: "Formas de pagamento",
-    items: [{ label: "Mastercard" }, { label: "Visa" }, { label: "Pix" }],
-  },
-  mobileApps = { apple: "/", android: "/" },
-  regionOptions = { currency: [], language: [] },
-  extraLinks = [],
-  backToTheTop,
-  layout = {
-    backgroundColor: "Primary",
-    variation: "Variation 1",
-    hide: {
-      logo: false,
-      newsletter: false,
-      sectionLinks: false,
-      socialLinks: false,
-      paymentMethods: false,
-      mobileApps: false,
-      regionOptions: false,
-      extraLinks: false,
-      backToTheTop: false,
+  socialIcons: [
+    {
+      image: "https://fakeimg.pl/28x28",
+      alt: "icon",
     },
+    {
+      image: "https://fakeimg.pl/28x28",
+      alt: "icon",
+    },
+  ],
+  sections: [
+    {
+      label: "LABEL",
+      items: [
+        {
+          href: "/",
+          label: "Item",
+        },
+        {
+          href: "/",
+          label: "Item",
+        },
+      ],
+    },
+    {
+      label: "LABEL",
+      items: [
+        {
+          href: "/",
+          label: "Item",
+        },
+        {
+          href: "/",
+          label: "Item",
+        },
+      ],
+    },
+    {
+      label: "LABEL",
+      items: [
+        {
+          href: "/",
+          label: "Item",
+        },
+        {
+          href: "/",
+          label: "Item",
+        },
+        {
+          href: "/",
+          label: "Item",
+        },
+      ],
+    },
+    {
+      label: "LABEL",
+      items: [
+        {
+          href: "/",
+          label: "Item",
+        },
+        {
+          href: "/",
+          label: "Item",
+        },
+        {
+          href: "/",
+          label: "Item",
+        },
+      ],
+    },
+  ],
+  payment: {
+    title: "PAGAMENTO",
+    icons: [
+      {
+        image: "https://fakeimg.pl/40x40",
+        alt: "icon",
+      },
+      {
+        image: "https://fakeimg.pl/40x40",
+        alt: "icon",
+      },
+      {
+        image: "https://fakeimg.pl/40x40",
+        alt: "icon",
+      },
+      {
+        image: "https://fakeimg.pl/40x40",
+        alt: "icon",
+      },
+      {
+        image: "https://fakeimg.pl/40x40",
+        alt: "icon",
+      },
+      {
+        image: "https://fakeimg.pl/40x40",
+        alt: "icon",
+      },
+      {
+        image: "https://fakeimg.pl/40x40",
+        alt: "icon",
+      },
+    ],
   },
-}: Props) {
-  const _logo = layout?.hide?.logo ? <></> : <Logo logo={logo} />;
-  const _newsletter = layout?.hide?.newsletter ? <></> : (
-    <Newsletter
-      content={newsletter}
-      layout={{
-        tiled: layout?.variation == "Variation 4" ||
-          layout?.variation == "Variation 5",
-      }}
-    />
-  );
-  const _sectionLinks = layout?.hide?.sectionLinks ? <></> : (
-    <FooterItems
-      sections={sections}
-      justify={layout?.variation == "Variation 2" ||
-        layout?.variation == "Variation 3"}
-    />
-  );
-  const _social = layout?.hide?.socialLinks
-    ? <></>
-    : <Social content={social} vertical={layout?.variation == "Variation 3"} />;
-  const _payments = layout?.hide?.paymentMethods
-    ? <></>
-    : <PaymentMethods content={payments} />;
-  const _apps = layout?.hide?.mobileApps
-    ? <></>
-    : <MobileApps content={mobileApps} />;
-  const _region = layout?.hide?.regionOptions
-    ? <></>
-    : <RegionSelector content={regionOptions} />;
-  const _links = layout?.hide?.extraLinks
-    ? <></>
-    : <ExtraLinks content={extraLinks} />;
+  security: {
+    title: "SITE SEGURO",
+    icons: [
+      {
+        image: "https://fakeimg.pl/110x32",
+        alt: "icon",
+      },
+    ],
+  },
+  poweredIcons: [
+    {
+      image: "https://fakeimg.pl/120x60",
+      alt: "icon",
+    },
+    {
+      image: "https://fakeimg.pl/63x43",
+      alt: "icon",
+    },
+  ],
+  textBottom:
+    "<p>Copyright @ Ótima Ajuda 2023 - Todos os direitos reservados.<br/>CNPJ 00.000.000/0000-00 / Endereço Exemplo teste, 1000 - Bairro, Estado - UF 00000-000, Brasil</p>",
+};
+
+function Footer(props: Props) {
+  const {
+    newsletter,
+    logo,
+    socialIcons,
+    sections,
+    payment,
+    security,
+    poweredIcons,
+    textBottom,
+  } = {
+    ...DEFAULT_PROPS,
+    ...props,
+  };
 
   return (
-    <footer
-      class={`w-full flex flex-col pt-10 pb-2 md:pb-10 gap-10 ${
-        ColorClasses(layout)
-      }`}
-    >
-      <div class="lg:container mx-6 lg:mx-auto">
-        {(!layout?.variation || layout?.variation == "Variation 1") && (
-          <div class="flex flex-col gap-10">
-            <div class="flex flex-col md:flex-row md:justify-between md:flex-wrap lg:flex-nowrap gap-8 lg:gap-12">
-              {_logo}
-              {_sectionLinks}
-              {_newsletter}
-            </div>
-            <Divider />
-            <div class="flex flex-col md:flex-row gap-10 md:gap-14 md:items-end">
-              {_payments}
-              {_social}
-              <div class="flex flex-col lg:flex-row gap-10 lg:gap-14 lg:items-end">
-                {_apps}
-                {_region}
+    <footer>
+      <div class="">
+        {
+          /* <div class="bg-[#1E274A]">
+          <Newsletter content={newsletter} />
+        </div> */
+        }
+        <div class="bg-[#2E385F] pt-[17px] pb-5 lg:px-5">
+          <div class="max-w-[1270px] mx-auto flex flex-col px-5 lg:flex-row lg:px-0 lg:mb-11">
+            <div class="lg:mr-[13.07%] flex flex-col items-center gap-[27px] mb-10 lg:mb-0 lg:items-start">
+              <img src={logo?.image} alt={logo?.alt} />
+              <div class="flex gap-5">
+                {socialIcons?.map((icon, index) => (
+                  <img src={icon.image} alt={icon.alt} key={index} />
+                ))}
               </div>
             </div>
-            <Divider />
-            <div class="flex flex-col-reverse md:flex-row md:justify-between gap-10">
-              <PoweredByDeco />
-              {_links}
+            <div>
+              <FooterItems sections={sections} />
             </div>
           </div>
-        )}
-        {layout?.variation == "Variation 2" && (
-          <div class="flex flex-col gap-10">
-            <div class="flex flex-col md:flex-row gap-10">
-              <div class="flex flex-col gap-10 lg:w-1/2">
-                {_logo}
-                {_social}
-                {_payments}
-                {_apps}
-                {_region}
-              </div>
-              <div class="flex flex-col gap-10 lg:gap-20 lg:w-1/2 lg:pr-10">
-                {_newsletter}
-                {_sectionLinks}
-              </div>
-            </div>
-            <Divider />
-            <div class="flex flex-col-reverse md:flex-row md:justify-between gap-10">
-              <PoweredByDeco />
-              {_links}
-            </div>
-          </div>
-        )}
-        {layout?.variation == "Variation 3" && (
-          <div class="flex flex-col gap-10">
-            {_logo}
-            <div class="flex flex-col lg:flex-row gap-14">
-              <div class="flex flex-col md:flex-row lg:flex-col md:justify-between lg:justify-normal gap-10 lg:w-2/5">
-                {_newsletter}
-                <div class="flex flex-col gap-10">
-                  {_payments}
-                  {_apps}
+          <div class="max-w-[1270px] mx-auto px-5 mb-5 lg:flex justify-between items-center lg:px-0 lg:mb-[54px]">
+            <div class="lg:flex gap-[95px]">
+              <div class="flex flex-col mb-7 lg:flex-row lg:items-center lg:gap-[25px] lg:mb-0">
+                <p class="text-white text-center text-[11px] leading-normal uppercase mb-3 lg:mb-0">
+                  {payment?.title}
+                </p>
+                <div class="flex justify-center gap-[10px]">
+                  {payment?.icons?.map((icon, index) => (
+                    <img src={icon.image} alt={icon.alt} key={index} />
+                  ))}
                 </div>
               </div>
-              <div class="flex flex-col gap-10 lg:gap-20 lg:w-3/5 lg:items-end">
-                <div class="flex flex-col md:flex-row gap-10">
-                  {_sectionLinks}
-                  {_social}
-                </div>
-                {_region}
-              </div>
-            </div>
-            <Divider />
-            <div class="flex flex-col-reverse md:flex-row md:justify-between gap-10">
-              <PoweredByDeco />
-              {_links}
-            </div>
-          </div>
-        )}
-        {layout?.variation == "Variation 4" && (
-          <div class="flex flex-col gap-10">
-            {_newsletter}
-            {layout?.hide?.newsletter ? <></> : <Divider />}
-            <div class="flex flex-col lg:flex-row gap-10 lg:gap-20 lg:justify-between">
-              {_sectionLinks}
-              <div class="flex flex-col md:flex-row lg:flex-col gap-10 lg:gap-10 lg:w-2/5 lg:pl-10">
-                <div class="flex flex-col md:flex-row gap-10 lg:gap-20">
-                  <div class="lg:flex-auto">
-                    {_payments}
-                  </div>
-                  <div class="lg:flex-auto">
-                    {_social}
-                  </div>
-                </div>
-                <div class="flex flex-col gap-10 lg:gap-10">
-                  {_region}
-                  {_apps}
+              <div class="flex flex-col mb-7 lg:flex-row lg:items-center lg:gap-[25px] lg:mb-0">
+                <p class="text-white text-center text-[11px] leading-normal uppercase mb-3 lg:mb-0">
+                  {security?.title}
+                </p>
+                <div class="flex justify-center gap-[10px]">
+                  {security?.icons?.map((icon, index) => (
+                    <img src={icon.image} alt={icon.alt} key={index} />
+                  ))}
                 </div>
               </div>
             </div>
-            <Divider />
-            <div class="flex flex-col md:flex-row md:justify-between gap-10 md:items-center">
-              {_logo}
-              <PoweredByDeco />
+            <div class="flex justify-center gap-10">
+              {poweredIcons?.map((icon, index) => (
+                <img src={icon.image} alt={icon.alt} key={index} />
+              ))}
             </div>
           </div>
-        )}
-        {layout?.variation == "Variation 5" && (
-          <div class="flex flex-col gap-10">
-            {_newsletter}
-            {layout?.hide?.newsletter ? <></> : <Divider />}
-            {_logo}
-            <div class="flex flex-col md:flex-row gap-10 lg:gap-20 md:justify-between">
-              {_sectionLinks}
-              <div class="flex flex-col gap-10 md:w-2/5 lg:pl-10">
-                {_payments}
-                {_social}
-                {_apps}
-              </div>
-            </div>
-            <Divider />
-            <div class="flex flex-col-reverse md:flex-row md:justify-between gap-10 md:items-center">
-              <PoweredByDeco />
-              <div class="flex flex-col md:flex-row gap-10 md:items-center">
-                {_links}
-                {_region}
-              </div>
-            </div>
+          <div class="px-5 flex justify-center lg:px-0">
+            {/* <RichText text={textBottom} /> */}
+            <p class="text-white text-[10px] leading-[18px] lg:text-center">
+              Copyright @ Ótima Ajuda 2023 - Todos os direitos reservados.
+              <br />
+              CNPJ 00.000.000/0000-00 / Endereço Exemplo teste, 1000 - Bairro,
+              Estado - UF 00000-000, Brasil
+            </p>
           </div>
-        )}
+        </div>
       </div>
-      {layout?.hide?.backToTheTop
-        ? <></>
-        : <BackToTop content={backToTheTop?.text} />}
     </footer>
   );
 }
--- components/footer/FooterItems.tsx
+++ components/footer/FooterItems.tsx
@@ -10,29 +10,26 @@ export type Section = {
   items: Item[];
 };
 
-export default function FooterItems(
-  { sections, justify = false }: { sections: Section[]; justify: boolean },
-) {
+export default function FooterItems({ sections }: { sections: Section[] }) {
   return (
     <>
       {sections.length > 0 && (
         <>
           {/* Tablet and Desktop view */}
-          <ul
-            class={`hidden md:flex flex-row gap-6 lg:gap-10 ${
-              justify && "lg:justify-between"
-            }`}
-          >
+          <ul class="hidden md:flex flex-row gap-6 lg:gap-10">
             {sections.map((section) => (
               <li>
                 <div class="flex flex-col gap-2">
-                  <span class="font-medium text-lg">
+                  <span class="font-medium text-lg text-white">
                     {section.label}
                   </span>
                   <ul class={`flex flex-col gap-2 flex-wrap text-sm`}>
                     {section.items?.map((item) => (
                       <li>
-                        <a href={item.href} class="block py-1 link link-hover">
+                        <a
+                          href={item.href}
+                          class="block py-1 link link-hover text-white"
+                        >
                           {item.label}
                         </a>
                       </li>
@@ -47,20 +44,18 @@ export default function FooterItems(
           <ul class="flex flex-col md:hidden gap-4">
             {sections.map((section) => (
               <li>
-                <div class="collapse collapse-arrow ">
+                <div class="collapse collapse-arrow">
                   <input type="checkbox" class="min-h-[0]" />
-                  <div class="collapse-title min-h-[0] !p-0 flex gap-2">
+                  <div class="collapse-title min-h-[0] !p-0 flex gap-2 text-white">
                     <span>{section.label}</span>
                   </div>
                   <div class="collapse-content">
-                    <ul
-                      class={`flex flex-col gap-1 pl-5 pt-2`}
-                    >
+                    <ul class={`flex flex-col gap-1 pl-5 pt-2`}>
                       {section.items?.map((item) => (
                         <li>
                           <a
                             href={item.href}
-                            class="block py-1 link link-hover"
+                            class="block py-1 link link-hover text-white"
                           >
                             {item.label}
                           </a>
