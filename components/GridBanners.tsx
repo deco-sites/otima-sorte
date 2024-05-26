0742b9a0971a91e140a537f94acac917c1f2e9d4
FelipeCSRosa <ffelipecs.rosa@gmail.com>
2024-05-26 14:38:20 -0300

    link banners

--- components/GridBanners.tsx
+++ components/GridBanners.tsx
@@ -38,18 +38,34 @@ const GridBanners = (props: GridBannersProps) => {
     <div class="px-[15px] lg:bg-[#F6F6F6] py-[26px] lg:pt-[58px] lg:pb-[50px]">
       <div class="max-w-[517px] mx-auto lg:max-w-[1270px] flex flex-col gap-[18px] lg:gap-[23px]">
         <div class="flex flex-col gap-[18px] lg:flex-row lg:gap-5">
-          <img src={topLeftMobile} alt="" class="lg:hidden min-w-0" />
-          <img src={topLeftDesktop} alt="" class="hidden lg:block min-w-0" />
-          <img src={topRight} alt="" class="min-w-0" />
+          <a href="/products/produto-1-47795223986478">
+            <img src={topLeftMobile} alt="" class="lg:hidden min-w-0" />
+          </a>
+          <a href="/products/produto-1-47795223986478">
+            <img src={topLeftDesktop} alt="" class="hidden lg:block min-w-0" />
+          </a>
+          <a href="/products/produto-1-47795223986478">
+            <img src={topRight} alt="" class="min-w-0" />
+          </a>
         </div>
         <div class="flex flex-col gap-[18px] lg:flex-row lg:gap-5">
-          <img src={middleLeft} alt="" class="min-w-0" />
-          <img src={middleCenter} alt="" class="min-w-0" />
-          <img src={middleRight} alt="" class="min-w-0" />
+          <a href="/products/produto-1-47795223986478">
+            <img src={middleLeft} alt="" class="min-w-0" />
+          </a>
+          <a href="/products/produto-1-47795223986478">
+            <img src={middleCenter} alt="" class="min-w-0" />
+          </a>
+          <a href="/products/produto-1-47795223986478">
+            <img src={middleRight} alt="" class="min-w-0" />
+          </a>
         </div>
         <div>
-          <img src={bottomMobile} alt="" class="lg:hidden" />
-          <img src={bottomDesktop} alt="" class="hidden lg:block" />
+          <a href="/products/produto-1-47795223986478">
+            <img src={bottomMobile} alt="" class="lg:hidden" />
+          </a>
+          <a href="/products/produto-1-47795223986478">
+            <img src={bottomDesktop} alt="" class="hidden lg:block" />
+          </a>
         </div>
       </div>
     </div>
